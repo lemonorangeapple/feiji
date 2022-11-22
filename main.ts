82e357796fe4379c4c611e5f2c69cb2217aefd77
mfwq <115348922+mfwq@users.noreply.github.com>
2022-11-22 15:36:59 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,35 +4,6 @@ enum RadioMessage {
 namespace SpriteKind {
     export const Boss = SpriteKind.create()
 }
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (boss == 0) {
-        _boss = sprites.create(assets.image`boss`, SpriteKind.Boss)
-        _boss_blood = 100
-        for (let index = 0; index < 10000; index++) {
-            _boss.setPosition(randint(0, 160), randint(0, 120))
-            pause(500)
-        }
-        boss = 1
-    }
-    if (boss == 1) {
-        _boss1 = sprites.create(assets.image`boss2`, SpriteKind.Boss)
-        _boss_blood = 1000
-        for (let index = 0; index < 10000; index++) {
-            _boss1.setPosition(randint(0, 160), randint(0, 120))
-            pause(500)
-        }
-        boss = 2
-    }
-    if (boss == 2) {
-        _boss2 = sprites.create(assets.image`boss3`, SpriteKind.Boss)
-        _boss_blood = 10000
-        for (let index = 0; index < 10000; index++) {
-            _boss2.setPosition(randint(0, 160), randint(0, 120))
-            pause(500)
-        }
-        boss = 3
-    }
-})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Boss, function (sprite, otherSprite) {
     info.changeLifeBy(-3)
     scene.cameraShake(4, 100)
@@ -46,6 +17,27 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         player_exp = sprites.createProjectileFromSprite(assets.image`player_exp`, player_plane, 100, 0)
         player_exp.setFlag(SpriteFlag.AutoDestroy, true)
         info.changeScoreBy(-1)
+    }
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    _boss = sprites.create(assets.image`boss`, SpriteKind.Boss)
+    _boss_blood = 100
+    _boss.setPosition(160, 20)
+    _boss1 = sprites.create(assets.image`boss2`, SpriteKind.Boss)
+    _boss_blood = 1000
+    _boss1.setPosition(160, 60)
+    if (_boss_blood <= 0) {
+        info.setScore(10000000000000000)
+        info.setLife(10000000000000000)
+        _boss1.destroy()
+    }
+    _boss2 = sprites.create(assets.image`boss3`, SpriteKind.Boss)
+    _boss_blood = 10000
+    _boss2.setPosition(160, 100)
+    if (_boss_blood <= 0) {
+        info.setScore(10000000000000000)
+        info.setLife(10000000000000000)
+        _boss2.destroy()
     }
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
@@ -71,16 +63,15 @@ controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 let _2nd_plane: Sprite = null
-let player_exp: Sprite = null
 let _boss2: Sprite = null
 let _boss1: Sprite = null
 let _boss: Sprite = null
+let player_exp: Sprite = null
 let player_plane: Sprite = null
 let _boss_blood = 0
-let boss = 0
 let b = 0
 b = 0
-boss = 0
+let boss = 0
 _boss_blood = 100000
 scene.setBackgroundColor(8)
 info.setLife(3)
@@ -94,23 +85,8 @@ forever(function () {
         info.setScore(10000000000000000)
         info.setLife(10000000000000000)
         _boss.destroy()
-        _boss1 = sprites.create(assets.image`boss2`, SpriteKind.Boss)
-        _boss_blood = 1000
-        for (let index = 0; index < 10000; index++) {
-            _boss1.setPosition(randint(0, 160), randint(0, 120))
-            pause(500)
-        }
-        boss = 2
         _boss1.destroy()
-        _boss2 = sprites.create(assets.image`boss3`, SpriteKind.Boss)
-        _boss_blood = 10000
-        for (let index = 0; index < 10000; index++) {
-            _boss2.setPosition(randint(0, 160), randint(0, 120))
-            pause(500)
-        }
-        boss = 3
         _boss2.destroy()
-        game.over(true, effects.confetti)
     }
 })
 game.onUpdateInterval(2000, function () {
